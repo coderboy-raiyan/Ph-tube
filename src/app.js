dca1a228aaceae61829d5989372a5cf5684e26f7
Raiyan <tajkierhaque@gmail.com>
2023-11-24 23:46:02 +0600

    some has been bug fixed

--- src/app.js
+++ src/app.js
@@ -50,7 +50,7 @@ async function handleVideoContent(id, idx) {
   categorySection.childNodes[idx].classList.add("text-white");
 
   loader.style.display = "flex";
-
+  videoContentArea.innerHTML = "";
   setTimeout(async () => {
     try {
       const request = await fetch(
@@ -58,64 +58,70 @@ async function handleVideoContent(id, idx) {
       );
       const response = await request.json();
       const { data } = await response;
-      if (!data.length) {
-        videoNotFound.classList.add("flex");
-        videoNotFound.classList.remove("hidden");
-      } else {
-        videoNotFound.classList.remove("flex");
-        videoNotFound.classList.add("hidden");
+      if (data.length) {
       }
-      videoContentArea.innerHTML = data
-        .map((vid) => {
-          return ` <div class="video-card">
-       <div class="relative">
-            <img
-            class="rounded-xl w-full lg:h-[200px] md:h-[200px] md:w-[312px] lg:w-[312px]"
-            src="${vid?.thumbnail}"
-            alt=""
-            />
-           ${
-             parseTime(vid?.others?.posted_date) &&
-             ` <span class="absolute bottom-[10px] py-1 px-2 rounded-md bg-[#171717] text-[12px] text-[#fff] right-[10px]">
-                ${parseTime(vid?.others?.posted_date)}
-              </span>`
-           }
-       </div>
-  
-        <div class="video-info flex gap-x-4 my-2">
-         <div class="avatar">
-            <div class="w-10 h-10 rounded-full">
-                <img
-                loading="lazy"
-                class="profile-img"
-                src="${vid?.authors[0]?.profile_picture}"
-                alt=""
-                />
-            </div>
-         </div>
-  
-          <div class="video-details flex flex-col gap-y-2">
-            <p class="font-semibold text-[#171717]">
-              ${vid?.title}
-            </p>
-            <div class="user-profile flex gap-x-2">
-              <h3 class="text-[#171717B2] text-[14px] font-medium">
-                ${vid?.authors[0]?.profile_name}
-              </h3>
+      if (!data.length) {
+        videoContentArea.innerHTML = ` <div
+          class="video-not-found col-span-4 min-h-[300px] justify-center items-center flex-col gap-y-6 text-3xl font-bold flex"
+        >
+          <img src="./imgs/Icon.png" alt="" />
+          <h1 class="lg:w-[28%] md:w-[28%] w-full mx-auto text-center">
+            Oops!! Sorry, There is no content here
+          </h1>
+        </div>`;
+      } else {
+        videoContentArea.innerHTML = data
+          .map((vid) => {
+            return ` <div class="video-card">
+         <div class="relative">
               <img
-                src="./imgs/verified.png"
-                alt=""
-                class="verified-icon w-5 h-5"
+              class="rounded-xl w-full lg:h-[200px] md:h-[200px] md:w-[312px] lg:w-[312px]"
+              src="${vid?.thumbnail}"
+              alt=""
               />
+             ${
+               parseTime(vid?.others?.posted_date) &&
+               ` <span class="absolute bottom-[10px] py-1 px-2 rounded-md bg-[#171717] text-[12px] text-[#fff] right-[10px]">
+                  ${parseTime(vid?.others?.posted_date)}
+                </span>`
+             }
+         </div>
+    
+          <div class="video-info flex gap-x-4 my-2">
+           <div class="avatar">
+              <div class="w-10 h-10 rounded-full">
+                  <img
+                  loading="lazy"
+                  class="profile-img"
+                  src="${vid?.authors[0]?.profile_picture}"
+                  alt=""
+                  />
+              </div>
+           </div>
+    
+            <div class="video-details flex flex-col gap-y-2">
+              <p class="font-semibold text-[#171717]">
+                ${vid?.title}
+              </p>
+              <div class="user-profile flex gap-x-2">
+                <h3 class="text-[#171717B2] text-[14px] font-medium">
+                  ${vid?.authors[0]?.profile_name}
+                </h3>
+                <img
+                  src="./imgs/verified.png"
+                  alt=""
+                  class="verified-icon w-5 h-5"
+                />
+              </div>
+              <p class="views text-[#171717B2] text-[14px] font-medium">
+                ${vid?.others?.views} views
+              </p>
             </div>
-            <p class="views text-[#171717B2] text-[14px] font-medium">
-              ${vid?.others?.views} views
-            </p>
           </div>
-        </div>
-      </div>`;
-        })
-        .join("");
+        </div>`;
+          })
+          .join("");
+      }
 
       loader.style.display = "none";
     } catch (error) {
